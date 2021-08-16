22fd233c9285ddd95cb5e803d73b9f33fe26f56d
Kajal-12-eng <85156373+Kajal-12-eng@users.noreply.github.com>
2021-08-16 22:54:28 +0530

    "added UC1 file "

--- Stack&Queue/src/com/stack_queue/UC1.java
+++ Stack&Queue/src/com/stack_queue/UC1.java
@@ -1,5 +1,17 @@
 package com.stack_queue;
 
+
 public class UC1 {
+		public static void main(String[] args)   
+		{  
+		Stack<Integer> stk= new Stack<>();  
+		boolean result = stk.empty();  
+		stk.push(70);  
+		stk.push(30);  
+		stk.push(56);  
+		
+		System.out.println("Elements in Stack: " + stk);  
+		result = stk.empty();  
+		}  
 
 }
